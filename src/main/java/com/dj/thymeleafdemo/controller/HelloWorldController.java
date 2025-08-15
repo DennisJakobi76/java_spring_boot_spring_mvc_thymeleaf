b99b1e08e5b9e3398d585291f60ea8936e12d5c3
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-08-15 11:57:20 +0200

    Adds new form processing methods and updates templates
    Introduces two new controller methods to process form data: one using `HttpServletRequest` and the other using `@RequestParam`, enhancing flexibility in handling user input.
    
    Updates the HTML form to point to the new method and modifies the template to display a custom message, improving usability and feedback for users.

--- src/main/java/com/dj/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/dj/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.dj.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -18,4 +21,38 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    // need a controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add the message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend v3! " + theName;
+
+        // add the message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
